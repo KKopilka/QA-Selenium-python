fce24840bd370b6201c22fac14271e2939513d41
Yana <yana.stepanckova@yandex.ru>
2022-03-30 10:41:10 +0300

    работа с alert

--- section 2/lesson2_step3.py
+++ section 2/lesson2_step3.py
@@ -8,28 +8,27 @@ def calc(x):
 link = "http://suninjuly.github.io/alert_accept.html"
 try:
 
-    browser = webdriver.Chrome()
-    browser.get(link)
+  browser = webdriver.Chrome()
+  browser.get(link)
 
-    button = browser.find_element_by_css_selector("[type='submit']")
-    button.click()
+  button = browser.find_element_by_css_selector("[type='submit']")
+  button.click()
 
-    confirm = browser.switch_to.alert
-    confirm.accept()
+  confirm = browser.switch_to.alert
+  confirm.accept()
 
-    element = browser.find_element_by_id('input_value')
-    x = element.text
-    y = calc(x)
+  element = browser.find_element_by_id('input_value')
+  x = element.text
+  y = calc(x)
 
-    element2 = browser.find_element_by_name('text')
-    element2.send_keys(y)
+  element2 = browser.find_element_by_name('text')
+  element2.send_keys(y)
 
-    button2 = browser.find_element_by_css_selector("[type='submit']")
-    button2.click()
+  button2 = browser.find_element_by_css_selector("[type='submit']")
+  button2.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(5)
-    # закрываем браузер после всех манипуляций
-    browser.quit()
-
+  # успеваем скопировать код за 30 секунд
+  time.sleep(5)
+  # закрываем браузер после всех манипуляций
+  browser.quit()
